bd75bf7f9868e815119bb23665d8d1c82f1e6a79
Juan Carlos Sosa Peña <juan.c.sosa.p@gmail.com>
2019-10-02 13:22:43 -0300

    Update

--- projects/conbefree.cl/js/ctrl.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("mainApp", []);
-
-app.controller("mainCtrl", ["$scope", function($scope) {
-  $scope.title = "Cabañas El Toro - Radal Siete Tazas";
-  $scope.menus = [{page:'index', title:'Inicio'}];
-}]);
-
-app.filter('numberLen', ()=>(num, len)=>(1e10 + "" + num).slice(-len));
